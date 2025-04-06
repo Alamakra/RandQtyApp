d5d02ae44abc667f1cdf32b4982345d0946f0809
Alamakra <uriuri740@gmail.com>
2025-04-06 18:50:45 +0200

    fixed css and js

--- script.js
+++ script.js
@@ -1,24 +1,9 @@
 const quotes = [
-  {
-    text: 'The only way to do great work is to love what you do.',
-    author: 'Steve Jobs',
-  },
-  {
-    text: 'Strive not to be a success, but rather to be of value.',
-    author: 'Albert Einstein',
-  },
-  {
-    text: 'The future belongs to those who believe in the beauty of their dreams.',
-    author: 'Eleanor Roosevelt',
-  },
-  {
-    text: 'Tell me and I forget. Teach me and I remember. Involve me and I learn.',
-    author: 'Benjamin Franklin',
-  },
-  {
-    text: 'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart.',
-    author: 'Helen Keller',
-  },
+  'The only way to do great work is to love what you do. - Steve Jobs',
+  'Strive not to be a success, but rather to be of value. - Albert Einstein',
+  'The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt',
+  'Tell me and I forget. Teach me and I remember. Involve me and I learn. - Benjamin Franklin',
+  'The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. - Helen Keller',
 ];
 
 const quoteElement = document.getElementById('quote');
@@ -26,10 +11,11 @@ const generateBtn = document.getElementById('generate-btn');
 
 function generateRandomQuote() {
   const randomIndex = Math.floor(Math.random() * quotes.length);
+
   const randomQuote = quotes[randomIndex];
   quoteElement.textContent = randomQuote;
 }
 
-generateBtn.addEventListener('click', generateRandomQuote());
+generateBtn.addEventListener('click', generateRandomQuote);
 
-generateRandomQuote();
+/**generateRandomQuote();*/
